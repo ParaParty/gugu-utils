19a537b2eb11119c71022291a2c23126f053c8f4
warmthdawn <warmth06@sina.com>
2021-01-12 23:24:56 +0800

    给配置文件添加部分注释

--- src/main/java/com/warmthdawn/mod/gugu_utils/config/PluginsConfig.java
+++ src/main/java/com/warmthdawn/mod/gugu_utils/config/PluginsConfig.java
@@ -3,10 +3,11 @@ package com.warmthdawn.mod.gugu_utils.config;
 import com.warmthdawn.mod.gugu_utils.GuGuUtils;
 import net.minecraftforge.common.config.Config;
 
-@Config(modid = GuGuUtils.MODID, name = "Plugins")
+@Config(modid = GuGuUtils.MODID, category = "Plugins")
 public class PluginsConfig {
     public static boolean ENABLE_BLOOD_MAGIC_SUPPORT = true;
     public static boolean ENABLE_NATURES_AURA_SUPPORT = true;
+    @Config.Comment("Note: This will disable all the features related to botania includes Mana Hatches!")
     public static boolean ENABLE_BOTANIA_SUPPORT = true;
     public static boolean ENABLE_ASTRAL_SORCERY_SUPPORT = true;
     public static boolean ENABLE_EMBERS_SUPPORT = true;
--- src/main/java/com/warmthdawn/mod/gugu_utils/config/TweaksConfig.java
+++ src/main/java/com/warmthdawn/mod/gugu_utils/config/TweaksConfig.java
@@ -4,23 +4,33 @@ import com.warmthdawn.mod.gugu_utils.GuGuUtils;
 import com.warmthdawn.mod.gugu_utils.botania.subtitle.SubTileEntropinnyumModified;
 import net.minecraftforge.common.config.Config;
 
-@Config(modid = GuGuUtils.MODID, name = "Tweaks")
+@Config(modid = GuGuUtils.MODID, category = "Tweaks")
 public class TweaksConfig {
 
+    @Config.Comment("Enable psi accelate trick")
     public static boolean ENABLE_ACCELATE_TRICK = true;
+    @Config.Comment("Set to false to disable all generate flower tweaks")
     public static boolean TWEAKS_GENERATORS = true;
+    @Config.Comment("Set to false to disable gaia player number tweaks")
     public static boolean TWEAKS_GAIA = true;
+    @Config.Comment("Set to false to disable all lens added")
     public static boolean TWEAKE_LENS = true;
 
     public static boolean ENABLE_LENS_OVERCLOCKING = true;
     public static boolean ENABLE_LENS_TRASNFORM = true;
 
+
+    @Config.Comment("Enable endoflame efficiency reduce")
     public static boolean TWEAK_ENDOFLAME_EFFICIENCY = true;
+    @Config.Comment("Max flowers that endoflame's efficiency begin to reduce")
     public static int ENDOFLAME_MAX_FLOWERS = 5;
 
+    @Config.Comment("Enable arcanerose efficiency reduce")
     public static boolean TWEAK_ARCANEROSE_EFFICIENCY = true;
+    @Config.Comment("Max flowers that arcanerose's efficiency begin to reduce")
     public static int ARCANEROSE_MAX_FLOWERS = 4;
 
+    @Config.Comment("Enable entropinnyum refuse to accept copied tnt")
     public static boolean ENTROPINNYUM_NOT_ACCEPT_COPY_TNT = true;
     public static int ENTROPINNYUM_GENERATING = 8000;
 
