e74566b17cd4e03dcc82fa9c4c6c3e0664a6a651
WarmthDawn <warmth06@sina.com>
2021-06-21 18:50:34 +0800

    Fix #3

--- src/main/java/com/warmthdawn/mod/gugu_utils/common/IGuiProvider.java
+++ src/main/java/com/warmthdawn/mod/gugu_utils/common/IGuiProvider.java
@@ -4,10 +4,13 @@ import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 
 public interface IGuiProvider {
     Container createContainer(EntityPlayer player);
 
+    @SideOnly(Side.CLIENT)
     GuiScreen createGui(EntityPlayer player);
 }
