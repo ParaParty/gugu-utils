82d34c05f0c0273f5ff461dc17f1929e475c89c2
WarmthDawn <warmth06@sina.com>
2021-09-27 17:45:41 +0800

    :bug: 修复仍然依赖build53模块化的bug

--- src/main/java/com/warmthdawn/mod/gugu_utils/modularmachenary/IColorableTileEntity.java
+++ src/main/java/com/warmthdawn/mod/gugu_utils/modularmachenary/IColorableTileEntity.java
@@ -1,8 +1,6 @@
 package com.warmthdawn.mod.gugu_utils.modularmachenary;
 
-import hellfirepvp.modularmachinery.common.tiles.base.ColorableMachineTile;
-
-public interface IColorableTileEntity extends ColorableMachineTile {
+public interface IColorableTileEntity {
     int getMachineColor();
 
     void setMachineColor(int newColor);
